bab405ca6b43ae1a0785fde1c6b49816f53e2109
shahariaz <shahariaz.info@gmail.com>
2025-08-29 16:06:17 +0600

    varibale

---
+++ src/intorTopic/Variable.java
@@ -0,0 +1,11 @@
+package intorTopic;
+
+public class Variable {
+  public static void main(String[] args){
+      //Assign value during declaration
+      int speeedLimit = 80;
+      //Java is a statically typed language. It means all variables must be declared before they can be used.
+      // variable/ container  and variable name / identifier use interchangeably
+      // Java is case-sensitive
+  }
+}
